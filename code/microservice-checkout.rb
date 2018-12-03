3e114d9a1941329d0b36e117f300e28a2c727bbc
JJ Merelo <jjmerelo@gmail.com>
2018-12-03 09:59:17 +0100

    Instalando en un usuario

--- code/microservice-checkout.rb
+++ code/microservice-checkout.rb
@@ -1,7 +1,20 @@
+user 'app' do
+  comment 'User for app'
+  home '/app'
+  shell '/bin/bash'
+  password '$1$JJsvHslasdfjVEroftprNn4JHtDi'
+end
+
+directory '/app' do
+  owner 'app'
+  group 'app'
+  mode '0755'
+end
+
 git 'microservice' do
   repository 'https://github.com/JJ/microservices-broker'
-  user 'vagrant'
-  group 'vagrant'
-  destination '/home/vagrant/microservice'
+  user 'app'
+  group 'app'
+  destination '/app'
   action :checkout
 end
